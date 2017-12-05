70836d41a1c1a8ab55ccc9e031e63334eee7a7dd
Daniel <daniel.maansson@gmail.com>
2017-12-05 09:54:18 +0100

    Initial commit. Day 1 - 4

---
+++ day3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetDist(1));
+			Console.WriteLine(GetDist(12));
+			Console.WriteLine(GetDist(23));
+			Console.WriteLine(GetDist(1024));
+			Console.WriteLine(GetDist(312051));
+
+			SolvePart2();
+
+			Console.ReadKey();
+		}
+
+		static int GetDist(int idx)
+		{
+			if (idx == 1)
+				return 0;
+
+			//find corner
+			int c = 1;
+			for (c = 1; c < SIZE; c += 2)
+			{
+				if (idx <= c * c)
+				{
+					break;
+				}
+			}
+
+			int distFromCorner = c * c - idx;
+			int v = c / 2;
+
+			int a = distFromCorner % v;
+			if ((distFromCorner / v) % 2 == 0)
+				a = v - a;
+
+			return a + v;
+		}
+
+		const int SIZE = 100;
+		static int SolvePart2()
+		{
+			var grid = new int[SIZE * SIZE];
+
+			int idx = SIZE * (SIZE/2) + SIZE/2;
+			grid[idx] = 1;
+
+			for (int c = 1; c < 1000; c += 2)
+			{
+				int steps = c - 1;
+				//up
+				for (int i = 0; i < steps - 1; i++)
+				{
+					idx += SIZE;
+					Fill(grid, idx);
+				}
+				//left
+				for (int i = 0; i < steps; i++)
+				{
+					idx -= 1;
+					Fill(grid, idx);
+				}
+				//down
+				for (int i = 0; i < steps; i++)
+				{
+					idx -= SIZE;
+					Fill(grid, idx);
+				}
+				//right
+				for (int i = 0; i < steps + 1; i++)
+				{
+					idx += 1;
+					Fill(grid, idx);
+				}
+			}
+
+			return 0;
+		}
+
+		static void Fill(int[] grid, int idx)
+		{
+			grid[idx] =
+				grid[idx + 1] +
+				grid[idx - 1] +
+				grid[idx + SIZE] +
+				grid[idx - SIZE] +
+				grid[idx + 1 - SIZE] +
+				grid[idx - 1 - SIZE] +
+				grid[idx + 1 + SIZE] +
+				grid[idx - 1 + SIZE];
+
+			Console.WriteLine(grid[idx]);
+
+			if (grid[idx] > 312051)
+			{
+				Console.WriteLine(grid[idx]);
+				throw new Exception("Whatever, time to bail, haha: " + grid[idx]);
+			}
+		}
+	}
+}
---
+++ day4/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Run());
+
+			Console.WriteLine(IsValid2(new List<string>()
+			{
+				"abcde", "xyz", "ecdab"
+			}));
+
+			Console.WriteLine(IsValid2(new List<string>()
+			{
+				"iiii", "oiii", "ooii", "oooi", "oooo"
+			}));
+			Console.WriteLine(Run2());
+
+			Console.ReadKey();
+		}
+
+		static int Run()
+		{
+			var indata = LoadData("indata.txt");
+			return Solve(indata);
+		}
+
+		static int Run2()
+		{
+			var indata = LoadData("indata.txt");
+			return Solve2(indata);
+		}
+
+		static List<List<string>> LoadData(string filename)
+		{
+			var lines = File.ReadAllLines(filename);
+			return lines.Select(l => l.Split(' ').ToList()).ToList();
+		}
+
+		static int Solve(List<List<string>> indata)
+		{
+			return indata.Count(pass => IsValid(pass));
+		}
+
+		static int Solve2(List<List<string>> indata)
+		{
+			return indata.Count(pass => IsValid2(pass));
+		}
+
+		static bool IsValid(List<string> pass)
+		{
+			var used = new List<string>();
+			foreach (var word in pass)
+			{
+				if (used.Contains(word))
+					return false;
+
+				used.Add(word);
+			}
+
+			return true;
+		}
+
+		static bool IsValid2(List<string> pass)
+		{
+			var used = new List<string>();
+
+			for (int i = 0; i < pass.Count; i++)
+			{
+				for (int j = i + 1; j < pass.Count; j++)
+				{
+					if (IsAnagrammable(pass[i], pass[j]))
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		static bool IsAnagrammable(string a, string b)//its a word
+		{
+			var al = a.ToList();
+			al.Sort();
+			var bl = b.ToList();
+			bl.Sort();
+
+			return al.SequenceEqual(bl);
+		}
+	}
+}
