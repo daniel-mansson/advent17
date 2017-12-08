8866af1572d7a270c363733c58e21f23cf4aee1d
Daniel <daniel.maansson@gmail.com>
2017-12-08 20:57:33 +0100

    Modified template and added day 8 and year 16 day 4

--- day8/Program.cs
+++ day8/Program.cs
@@ -11,7 +11,10 @@ namespace day8
 		static int s_year = 2017;
 		static int s_day = 8;
 		static string s_example =
-@"";
+@"b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10";
 
 		static void Main(string[] args)
 		{
@@ -24,23 +27,135 @@ namespace day8
 			Console.ReadKey();
 		}
 
-		static string Transform(string raw)
+		class Statement
 		{
-			return raw;
+			public string Register;
+			public int Value;
+			public string Operation;
+			public string Condition;
+			public string CmpLhs;
+			public string CmpRhs;
+
+			public int PerformOperation(int lhs, int rhs)
+			{
+				switch (Operation)
+				{
+					case "inc":
+						return lhs + rhs;
+					case "dec":
+						return lhs - rhs;
+				}
+
+				throw new Exception("Unsupported: " + Operation);
+			}
+
+			public bool EvaluateCondition(int lhs, int rhs)
+			{
+				switch (Condition)
+				{
+					case ">":
+						return lhs > rhs;
+					case ">=":
+						return lhs >= rhs;
+					case "<":
+						return lhs < rhs;
+					case "<=":
+						return lhs <= rhs;
+					case "!=":
+						return lhs != rhs;
+					case "==":
+						return lhs == rhs;
+					default:
+						throw new Exception("Unsupported: " + Condition);
+				}
+			}
+		}
+
+		static List<Statement> Transform(string raw)
+		{
+			return raw
+				.Split('\n')
+				.Select(l =>
+				{
+					var s = l.Split(' ');
+
+					return new Statement()
+					{
+						Register = s[0],
+						Operation = s[1],
+						Value = int.Parse(s[2]),
+						Condition = s[5],
+						CmpLhs = s[4],
+						CmpRhs = s[6]
+					};
+				}).ToList();
 		}
 
 		static int Solve(string raw)
 		{
+			var memory = new Dictionary<string, int>();
 			var input = Transform(raw);
 
-			return -1;
+			foreach (var statement in input)
+			{
+				if (statement.EvaluateCondition(
+					ToValue(memory, statement.CmpLhs),
+					ToValue(memory, statement.CmpRhs)))
+				{
+					int result = statement.PerformOperation(
+						ToValue(memory, statement.Register),
+						statement.Value);
+
+					memory[statement.Register] = result;
+				}
+			}
+
+			int max = memory.Max(kvp => kvp.Value);
+
+			return max;
+		}
+
+		static int ToValue(Dictionary<string, int> memory, string id)
+		{
+			int value;
+			if (int.TryParse(id, out value))
+			{
+				return value;
+			}
+			else
+			{
+				if (!memory.ContainsKey(id))
+				{
+					memory[id] = 0;
+				}
+
+				return memory[id];
+			}
 		}
 
 		static int Solve2(string raw)
 		{
+			var memory = new Dictionary<string, int>();
 			var input = Transform(raw);
 
-			return -1;
+			int max = 0;
+
+			foreach (var statement in input)
+			{
+				if (statement.EvaluateCondition(
+					ToValue(memory, statement.CmpLhs),
+					ToValue(memory, statement.CmpRhs)))
+				{
+					int result = statement.PerformOperation(
+						ToValue(memory, statement.Register),
+						statement.Value);
+
+					memory[statement.Register] = result;
+					max = Math.Max(max, result);
+				}
+			}
+
+			return max;
 		}
 	}
 }
--- year16day4/Program.cs
+++ year16day4/Program.cs
@@ -11,36 +11,98 @@ namespace year16day4
 		static int s_year = 2016;
 		static int s_day = 4;
 		static string s_example =
-@"";
+@"aaaaa-bbb-z-y-x-123[abxyz]
+a-b-c-d-e-f-g-h-987[abcde]
+not-a-real-room-404[oarel]
+totally-real-room-200[decoy]
+qzmt-zixmtkozy-ivhz-343[aaaaa]";
 
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Example: " + Solve(s_example));
 			Console.WriteLine("Real: " + Solve(ProblemInput.FetchBlocking(s_year, s_day)));
 
-			Console.WriteLine("Example 2: " + Solve2(s_example));
 			Console.WriteLine("Real 2: " + Solve2(ProblemInput.FetchBlocking(s_year, s_day)));
 
 			Console.ReadKey();
 		}
 
-		static string Transform(string raw)
+		class Entry
 		{
-			return raw;
+			public List<string> words;
+			public int sector;
+			public string checksum;
+
+			public bool IsValid()
+			{
+				var dict = new Dictionary<char, int>();
+
+				foreach (var word in words)
+				{
+					foreach (var c in word)
+					{
+						if (!dict.ContainsKey(c))
+							dict[c] = 0;
+
+						dict[c]++;
+					}
+				}
+
+				var generated = string.Join("", dict.OrderByDescending(kvp => kvp.Value * 10000 + 'z' - kvp.Key).Take(5).Select(kvp => kvp.Key));
+				return generated == checksum;
+			}
+		}
+
+		static List<Entry> Transform(string raw)
+		{
+			return raw.Split('\n')
+				.Select(l => 
+				{
+					var s = l
+						.Replace("[", ",")
+						.Replace("]", "")
+						.Replace("\r", "")
+						.Replace("-", ",")
+						.Split(',')
+						.ToList();
+
+					return new Entry()
+					{
+						words = s.Take(s.Count - 2).ToList(),
+						checksum = s.Last(),
+						sector = int.Parse(s[s.Count - 2])
+					};
+				}).ToList();
 		}
 
 		static int Solve(string raw)
 		{
 			var input = Transform(raw);
 
-			return -1;
+			return input.Sum(i => i.IsValid() ? i.sector : 0);
 		}
 
 		static int Solve2(string raw)
 		{
 			var input = Transform(raw);
 
-			return -1;
+			var decrypted = input
+				.Where(i => i.IsValid())
+				.Select(i =>
+				{
+					return new KeyValuePair<string, int>(string.Join(" ",
+						i.words
+						.Select(w => string.Join("", w.Select(c => (char)((c - 'a' + i.sector) % ('z' - 'a' + 1) + 'a'))))),
+						i.sector);
+				})
+				.ToList();
+
+			var northpoles = decrypted.Where(kvp => kvp.Key.Contains("north")).ToList();
+
+			if (northpoles.Count != 1)
+				throw new Exception();
+
+			return northpoles[0].Value;
 		}
 	}
 }
