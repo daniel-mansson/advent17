d864923682110f391ce178388275809a4cfbd244
Daniel <daniel.maansson@gmail.com>
2017-12-12 13:10:50 +0100

    Added day 12

--- day12/Program.cs
+++ day12/Program.cs
@@ -11,7 +11,13 @@ namespace day12
 		static int s_year = 2017;
 		static int s_day = 12;
 		static string s_example =
-@"";
+@"0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
 
 		static void Main(string[] args)
 		{
@@ -24,23 +30,131 @@ namespace day12
 			Console.ReadKey();
 		}
 
-		static string Transform(string raw)
+		class Map
 		{
-			return raw;
+			public Dictionary<int, List<int>> m_connections = new Dictionary<int, List<int>>();
+
+			public void AddConnection(int a, int b)
+			{
+				List<int> l;
+				if (!m_connections.TryGetValue(a, out l))
+				{
+					l = new List<int>();
+					m_connections[a] = l;
+				}
+				if (!l.Contains(b))
+					l.Add(b);
+
+				if (!m_connections.TryGetValue(b, out l))
+				{
+					l = new List<int>();
+					m_connections[b] = l;
+				}
+				if (!l.Contains(a))
+					l.Add(a);
+			}
+
+			public List<int> GetConnections(int a)
+			{
+				List<int> l;
+				if (!m_connections.TryGetValue(a, out l))
+					l = new List<int>();
+
+				return l;
+			}
+		}
+
+		static Map Transform(string raw)
+		{
+			var nodeList = raw
+				.Replace(" <-> ", ", ")
+				.Replace(", ", ",")
+				.Split('\n')
+				.Select(s => s
+					.Split(',')
+					.Select(i => int.Parse(i))
+					.ToList())
+				.ToList();
+
+			var map = new Map();
+
+			foreach (var node in nodeList)
+			{
+				int from = node[0];
+				for (int i = 0; i < node.Count; i++)
+				{
+					map.AddConnection(from, node[i]);
+				}
+			}
+
+			return map;
 		}
 
 		static int Solve(string raw)
 		{
-			var input = Transform(raw);
+			var map = Transform(raw);
+			List<int> queue = new List<int>();
 
-			return -1;
+			int current = 0;
+			queue.Add(0);
+
+			while (current < queue.Count)
+			{
+				var connections = map.GetConnections(queue[current]);
+
+				foreach (var c in connections)
+				{
+					if (!queue.Contains(c))
+					{
+						queue.Add(c);
+					}
+				}
+
+				current++;
+			}
+
+			return current;
 		}
 
 		static int Solve2(string raw)
 		{
-			var input = Transform(raw);
+			var map = Transform(raw);
+			List<int> visited = new List<int>();
+			int groupCount = 0;
+			var values = map.m_connections.Keys.ToList();
 
-			return -1;
+			for (int i = 0; i < values.Count; i++)
+			{
+				int start = values[i];
+
+				if (!visited.Contains(start))
+				{
+					groupCount++;
+
+					int current = 0;
+					List<int> queue = new List<int>();
+					queue.Add(start);
+
+					while (current < queue.Count)
+					{
+						var connections = map.GetConnections(queue[current]);
+
+						foreach (var c in connections)
+						{
+							if (!queue.Contains(c))
+							{
+								queue.Add(c);
+							}
+						}
+
+						current++;
+					}
+
+					visited.AddRange(queue);
+				}
+			}
+
+			return groupCount;
 		}
 	}
 }
