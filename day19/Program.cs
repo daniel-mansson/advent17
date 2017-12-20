3d4846b011119247ec03555923a2af0446a2347d
Daniel <daniel.maansson@gmail.com>
2017-12-20 08:18:47 +0100

    Added day 19 and 20

--- day19/Program.cs
+++ day19/Program.cs
@@ -11,7 +11,12 @@ namespace day19
 		static int s_year = 2017;
 		static int s_day = 19;
 		static string s_example =
-@"";
+@"     |          
+     |  +--+    
+     A  |  C    
+ F---|----E|--+ 
+     |  |  |  D 
+     +B-+  +--+ ";
 
 		static void Main(string[] args)
 		{
@@ -24,23 +29,114 @@ namespace day19
 			Console.ReadKey();
 		}
 
-		static string Transform(string raw)
+		class Map
 		{
-			return raw;
+			public struct Vec2
+			{
+				public int x;
+				public int y;
+			}
+
+			public int m_width;
+			public int m_height;
+			public List<List<char>> m_data;
+			public List<char> m_memory = new List<char>();
+
+			public Vec2 m_pos;
+			public int m_dir = 3;
+			public int m_steps = 0;
+
+			public List<Vec2> m_dirs = new List<Vec2>()
+			{
+				new Vec2(){ x = 1, y = 0 },
+				new Vec2(){ x = 0, y = -1 },
+				new Vec2(){ x = -1, y = 0 },
+				new Vec2(){ x = 0, y = 1 },
+			};
+
+			public Map(List<List<char>> chars)
+			{
+				m_width = chars[0].Count;
+				m_height = chars.Count;
+				m_data = chars;
+
+				m_pos.x = m_data[0].IndexOf('|');
+			}
+
+			public char Get(int x, int y)
+			{
+				if (x < 0 || x >= m_width || y < 0 || y >= m_height)
+					return ' ';
+
+				return m_data[y][x];
+			}
+
+			public char Step()
+			{
+				++m_steps;
+				var d = m_dirs[m_dir];
+				m_pos.x += d.x;
+				m_pos.y += d.y;
+
+				var value = Get(m_pos.x, m_pos.y);
+				if (value == '+')
+				{
+					//Time to turn yo!
+					int uncoolDir = (m_dir + 2) % 4;
+					for (int i = 0; i < 4; i++)
+					{
+						if (i == uncoolDir)
+							continue;
+
+						var nd = m_dirs[i];
+						var next = Get(m_pos.x + nd.x, m_pos.y + nd.y);
+
+						if (next != ' ')
+						{
+							m_dir = i;
+							break;
+						}
+					}
+				}
+
+				if (value >= 'A' && value <= 'Z')
+				{
+					m_memory.Add(value);
+				}
+
+				return value;
+			}
 		}
 
-		static int Solve(string raw)
+		static Map Transform(string raw)
 		{
-			var input = Transform(raw);
+			var chars = raw
+				.Replace("\r", "")
+				.Split('\n')
+				.Select(l => l.ToList())
+				.ToList();
 
-			return -1;
+			return new Map(chars);
+		}
+
+		static string Solve(string raw)
+		{
+			var maze = Transform(raw);
+
+			while (maze.Step() != ' ')
+			{ }
+
+			return string.Join("", maze.m_memory);
 		}
 
 		static int Solve2(string raw)
 		{
-			var input = Transform(raw);
+			var maze = Transform(raw);
 
-			return -1;
+			while (maze.Step() != ' ')
+			{ }
+
+			return maze.m_steps;
 		}
 	}
 }
--- day20/Program.cs
+++ day20/Program.cs
@@ -11,7 +11,8 @@ namespace day20
 		static int s_year = 2017;
 		static int s_day = 20;
 		static string s_example =
-@"";
+@"p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>
+p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>";
 
 		static void Main(string[] args)
 		{
@@ -24,23 +25,152 @@ namespace day20
 			Console.ReadKey();
 		}
 
-		static string Transform(string raw)
+		class Particle
 		{
-			return raw;
+			public long x, y, z, vx, vy, vz, ax, ay, az;
+
+			public Particle(string raw)
+			{
+				var l = raw
+					.Replace("p=<", "")
+					.Replace("v=<", "")
+					.Replace("a=<", "")
+					.Replace(" ", "")
+					.Replace(">", "")
+					.Replace("\r", "")
+					.Split(',')
+					.Select(i => int.Parse(i))
+					.ToList();
+
+				x = l[0];
+				y = l[1];
+				z = l[2];
+				vx = l[3];
+				vy = l[4];
+				vz = l[5];
+				ax = l[6];
+				ay = l[7];
+				az = l[8];
+			}
+
+			public long GetDistance()
+			{
+				return Math.Abs(x) + Math.Abs(y) + Math.Abs(z);
+			}
+
+			public void Step()
+			{
+				vx += ax;
+				vy += ay;
+				vz += az;
+				x += vx;
+				y += vy;
+				z += vz;
+			}
+		}
+
+		static List<Particle> Transform(string raw)
+		{
+			return raw
+				.Split('\n')
+				.Select(l => new Particle(l))
+				.ToList();
 		}
 
 		static int Solve(string raw)
 		{
 			var input = Transform(raw);
 
-			return -1;
+			int minI = -1;
+
+			for (int i = 0; i < 20; i++)
+			{
+				minI = StepAndGetClosest(input, 1000);
+				Console.Write(minI + " ");
+			}
+			Console.WriteLine();
+
+			return minI;
+		}
+
+		static int StepAndGetClosest(List<Particle> input, int steps)
+		{
+			foreach (var p in input)
+			{
+				for (int i = 0; i < steps; i++)
+				{
+					p.Step();
+				}
+			}
+
+			long minD = long.MaxValue;
+			int minI = -1;
+			for (int i = 0; i < input.Count; i++)
+			{
+				long d = input[i].GetDistance();
+				if (d < minD)
+				{
+					minI = i;
+					minD = d;
+				}
+			}
+
+			return minI;
+		}
+		static int StepAndKill(List<Particle> input, int steps)
+		{
+			for (int i = 0; i < steps; i++)
+			{
+				foreach (var p in input)
+				{
+					p.Step();
+				}
+
+				var lookup = new Dictionary<long, int>();
+				var collisions = new List<int>();
+				for (int j = 0; j < input.Count; j++)
+				{
+					var p = input[j];
+					int other;
+					long hash = p.z * 1000000 * 1000000 + p.y * 1000000 + p.z;
+					if (lookup.TryGetValue(hash, out other))
+					{
+						if (!collisions.Contains(j))
+							collisions.Add(j);
+						if (!collisions.Contains(other))
+							collisions.Add(other);
+					}
+					else
+					{
+						lookup.Add(hash, j);
+					}
+				}
+
+				collisions.Sort();
+
+				for (int j = collisions.Count - 1; j >= 0; j--)
+				{
+					input.RemoveAt(collisions[j]); 
+				}
+			}
+
+			return input.Count;
 		}
 
 		static int Solve2(string raw)
 		{
 			var input = Transform(raw);
 
-			return -1;
+			int minI = -1;
+
+			for (int i = 0; i < 20; i++)
+			{
+				minI = StepAndKill(input, 1000);
+				Console.Write(minI + " ");
+			}
+			Console.WriteLine();
+
+			return minI;
 		}
 	}
 }
